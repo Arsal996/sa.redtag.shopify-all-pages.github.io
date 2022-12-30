4cd56dee0e1ee218f6af8accf85509528def1f64
Arslan Ahmad <115068086+Arsal996@users.noreply.github.com>
2022-12-30 15:23:13 +0500

    Delete Navbar directory

--- Navbar/js/navbar.js
+++
@@ -1,7 +0,0 @@
-const hamBurger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-
-hamBurger.addEventListener('click', function() {
-    hamBurger.classList.toggle('closeburger');
-    navMenu.classList.toggle('active');
-})
